a2e87afe24bcb3c6d5b614139f4de8ac530c432a
Marcos Avaca <avacamarcos4@gmail.com>
2026-09-13 19:02:53 -0300

    Se fixeo cosas en la calculadora xd

--- 01-calculadora.js
+++ 01-calculadora.js
@@ -5,13 +5,20 @@ const rl=readline.createInterface({
     output:process.stdout})
 async function pedirdatos()
 {
-    const operando = await rl.question("\nIngrese la operación (suma, resta, multiplicacion, division) :) \n");
+    let error=false;
+    const operando = (await rl.question("\nIngrese la operación (suma, resta, multiplicacion, division) :) \n")).toLowerCase().trim();
 
     const entradastring=await rl.question("Ingrese todos los operandores seperados por , ejemplo: 10,5,2 \n");
     const numerostring=entradastring.split(",");
     for(let i=0;i<numerostring.length;i++)
     {
     numerostring[i]=Number(numerostring[i].trim());
+    if (isNaN(numerostring[i])) // si no es numero.
+    {
+        console.log("Ingrese numeros, intente de nuevo! \n");
+        rl.close();
+        return;
+    }
     }
     let resultado;
     switch(operando)
@@ -40,6 +47,7 @@ async function pedirdatos()
         break;
     case "division":
         resultado=numerostring[0];
+        let errorDiv = false;
         for(let i=1;i<numerostring.length;i++)
         {
         if(numerostring[i]!=0)
@@ -49,6 +57,12 @@ async function pedirdatos()
         else
         {
         console.log("No se puede dividir por 0!");
+        errorDiv = true;
+        if(errorDiv)
+        {
+            rl.close();
+            return;
+        }
         break;  
         }
         }
@@ -56,9 +70,16 @@ async function pedirdatos()
          
     default:
         resultado="Ingrese una opcion valida, Intente de nuevo \n";
+        error=true;
         break;
     }
-    console.log(`El resultado es: ${resultado}`);
+    if(error)
+    {
+        console.log(resultado);
+    }
+    else{
+        console.log(`El resultado es: ${resultado}`);
+    }
     rl.close();
 }
 
